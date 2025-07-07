ee3fbe9f925ae164b4e8b420313be5495bd642e3
Armless Master <71901674+DimaShevchenkoo@users.noreply.github.com>
2025-07-08 00:06:36 +0300

    Added plugin

---
+++ Source/ScreenshotAssistant/Public/SA_ScreenshotAction.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "SA_ScreenshotAction.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SCREENSHOTASSISTANT_API USA_ScreenshotAction : public UBlueprintAsyncActionBase
+{
+    GENERATED_BODY()
+
+
+};
